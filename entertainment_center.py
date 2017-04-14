82872efcd446577626d59b10bd7fb4ee7d64238a
Connor McKinnon <connor.mckinnon@senecacollege.ca>
2017-04-14 16:51:58 -0400

    Add first instance of movie object

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,3 +1,8 @@
 import media
 
-unbreakable = media.Movie()
+unbreakable = media.Movie("Unbreakable",
+                         "A man survives a horrific train crash. After the incident, with the help of a comic book shop owner, he learns that he may ne more than human",
+                         "https://upload.wikimedia.org/wikipedia/en/9/9e/Unbreakableposterwillis.jpg",
+                         "https://www.youtube.com/watch?v=HGrN6blSCGw")
+
+print unbreakable.storyline

90e6a7b92c7fc57be43c9a47eb5c526b954c5335
Connor McKinnon <connor.mckinnon@senecacollege.ca>
2017-04-15 11:33:32 -0400

    Update to match PEP8 recommendations

--- media.py
+++ media.py
@@ -1,10 +1,11 @@
 # This code was written while watching the associated course materials
 # I have refactored some in order to simplify production code
-# Other potential expansions of this code learned in the course can be found on the tv_shows branch
+# Expansions learned in the course on the tv_shows branch
 
 
-# From the Python Standard Library and contains methods to work with the browser
+# From the Python Standard Library - method to work with browser
 import webbrowser
+
 
 # Refactored to create parent class for later use in incorporating TV Shows
 class Video():
@@ -12,17 +13,18 @@ class Video():
         self.title = title
         self.duration = duration
 
+
 # Movie class used to save information about movies
 class Movie(Video):
-    
+
     """ This class stores movie related information """
-    
-    def __init__(self, title, movie_storyline, poster_image, trailer_youtube, duration = None):
-        
+
+    def __init__(self, title, storyline, image, youtube, duration=None):
+
         # Inherits from the Video class and sets duration to none by default
         Video.__init__(self, title, duration)
-        
+
         # Initializes the instance variables for this class
-        self.storyline = movie_storyline
-        self.poster_image_url = poster_image
-        self.trailer_youtube_url = trailer_youtube
+        self.storyline = storyline
+        self.poster_image_url = image
+        self.trailer_youtube_url = youtube
